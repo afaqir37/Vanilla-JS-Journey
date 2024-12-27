68b1e4669d6a6f9e69fd8b6f0fe8744761ffd4b7
Abdellah Faqir <afaqir@e1r9p10.1337.ma>
2024-12-27 19:02:22 +0100

    working on TIC-TAC-TOE after loading

--- Projects/Loading Bar/app.js
+++ Projects/Loading Bar/app.js
@@ -1,18 +1,38 @@
 const progress = document.getElementsByClassName('progress')
 const percentage = document.getElementById('percentage')
 
-const interval = setInterval(updateProgress, 100);
+//let interval = setInterval(updateProgress, 50);
 let width = 0
 
 
 function updateProgress() {
 
-    if (width == 100)
+    if (width == 100) {
         clearInterval(interval)
+    }
 
+    if (width != 100 && width >= 90) {
+        clearInterval(interval)
+        interval = setInterval(updateProgress, 100)
+    }
+
+    if (width != 100 && width >= 97) {
+        clearInterval(interval)
+        interval = setInterval(updateProgress, 300)
+    }
+
+    if (width != 100 && width == 99) {
+        clearInterval(interval)
+        interval = setInterval(updateProgress, 700)
+    }
     progress[0].style.width = width + '%'
     percentage.textContent = width 
 
     width++
 }
+
+
+
+
+
 
